e75dc3a6d6d8881ddf911ffa43dc8206ed60f37b
MALiceralde <146396314+MALiceralde@users.noreply.github.com>
2023-10-13 18:52:36 +0800

    Add files via upload

---
+++ right_triangle.py
@@ -0,0 +1,9 @@
+# The number of rows for the pattern
+num_rows = 5
+
+# Repetition Control Structure & Decision Control Structure
+for loop1 in range(1, num_rows + 1):
+
+    for loop2 in range(1, loop1 + 1):
+        print(loop2, end=' ')
+    print()
